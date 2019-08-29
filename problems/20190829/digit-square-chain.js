597fe16688966066f8d7ec21fb5b362f76728610
Teruo Kunihiro <yuyuyu1999@gmail.com>
2019-08-29 13:00:20 +0900

    Update

--- problems/20190829/digit-square-chain.js
+++ problems/20190829/digit-square-chain.js
@@ -1,33 +1,39 @@
-function countDigitSquareChain(n){
-    let resultMap = new Array(n)
-    let counter = 0
-
-    for(let i = 1; i < n; i++) {
-        if(resultMap[i] != null) {
-            if(resultMap[i] === 89) {
-                counter += 1
-            }
-            continue;
-        }
-        let tempArray = []
-        let squareChain = i
-        while(squareChain !== 89 && squareChain !== 1) {
-            tempArray.push(squareChain)
-            squareChain = squareChain.toString().split("").reduce((acc, cur) =>  {
-                return acc + Math.pow(parseInt(cur), 2)
-            },0)
-        }
-        for(const t of tempArray) {
-            resultMap[t] = squareChain
-        }
-
-        if(squareChain === 89) {
-            counter += 1
-        }
-        resultMap[i] = squareChain
+function calc(value) {
+    const str = value + "";
+    const digitNUm = (str).length;
+    let res = 0;
+    for (var i = 0; i < digitNUm; i++) {
+       res += Math.pow(parseInt(str[i]), 2);
     }
-
-    return counter
+    return res;
 }
 
-console.log(countDigitSquareChain(10000000))
+function chainDigit(value) {
+   let res = 0;
+   const map1 = new Map();
+   const map89 = new Map();
+   for (var i = 1; i < value; i++) {
+       let val = calc(i);
+       let isContinue = true;
+       while (isContinue) {
+           if (val === 1) {
+               map1.set(val, true);
+               isContinue = false;
+           } else if (val === 89) {
+               map89.set(val, true);
+               res++;
+               isContinue = false;
+           } else if (map1.has(val)) {
+               map1.set(val, true);
+               isContinue = false;
+           } else if (map89.has(val)) {
+               map89.set(val, true);
+               res++;
+               isContinue = false;
+           }
+           val = calc(val);
+       }
+   }
+   return res;
+}
+console.log("result", chainDigit(10000000));
